e7fa43eb80d8bf966178d3b162369310d42e5a43
gpm22 <gabriel.milhomem@vorasys.com.br>
2021-11-25 17:56:46 -0300

    fazendo o Componentizing our React app

--- React/GettingStartedWithReact/moz-todo-react/src/index.js
+++ React/GettingStartedWithReact/moz-todo-react/src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const DATA = [
+  {id: "todo-0", name: "Eat", completed: true},
+  {id: "todo-1", name: "Sleep", completed: false},
+  {id: "todo-2", name: "Repeat", completed: false}
+];
+
 ReactDOM.render(
-    <App subject="Eleonor"/>,
+    <App tasks={DATA}/>,
   document.getElementById('root')
 );
 
