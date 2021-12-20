cdcabe497964d831e05ab6b9916dd66e041487a8
gpm22 <gabriel.milhomem@vorasys.com.br>
2021-12-20 18:59:38 -0300

    adicionando estados e hooks

--- React/GettingStartedWithReact/moz-todo-react/src/App.js
+++ React/GettingStartedWithReact/moz-todo-react/src/App.js
@@ -1,29 +1,56 @@
 import Todo from "./components/Todo";
 import FilterButton from "./components/FilterButton";
 import Form from "./components/Form";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
 
-  const taskList = props.tasks.map(task => <Todo
-    id={task.id}
-    name={task.name}
-    completed={task.completed}
-    key={task.id}
-  />);
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      if(id=== task.id){
+        return {...task, complete: !task.completed};
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  const taskList = tasks.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+    />
+  ));
 
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-       <FilterButton />
-       <FilterButton />
-       <FilterButton />
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+        {taskList.length} {taskList.length !== 1 ? "tasks" : "task"} remaining
       </h2>
-      <ul 
+      <ul
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
--- React/GettingStartedWithReact/moz-todo-react/src/components/Todo.js
+++ React/GettingStartedWithReact/moz-todo-react/src/components/Todo.js
@@ -1,22 +1,31 @@
 import React from "react";
 
-export default function Todo(props){
-    return (
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id={props.id} type="checkbox" defaultChecked={props.completed} />
-            <label className="todo-label" htmlFor="todo-0">
-              {props.name}
-            </label>
-          </div>
-          <div className="btn-group">
-              <button type="button" className="btn">
-                Edit <span className="visually-hidden">{props.name}</span>
-              </button>
-              <button type="button" className="btn btn__danger">
-                    Delete <span className="visually=hidden">{props.name}</span>
-              </button>
-          </div>
-        </li>
-    );
+export default function Todo(props) {
+  return (
+    <li className="todo stack-small">
+      <div className="c-cb">
+        <input
+          id={props.id}
+          type="checkbox"
+          defaultChecked={props.completed}
+          onChange={() => props.toggleTaskCompleted(props.id)}
+        />
+        <label className="todo-label" htmlFor="todo-0">
+          {props.name}
+        </label>
+      </div>
+      <div className="btn-group">
+        <button type="button" className="btn">
+          Edit <span className="visually-hidden">{props.name}</span>
+        </button>
+        <button
+          type="button"
+          className="btn btn__danger"
+          onClick={() => props.deleteTask(props.id)}
+        >
+          Delete <span className="visually=hidden">{props.name}</span>
+        </button>
+      </div>
+    </li>
+  );
 }
