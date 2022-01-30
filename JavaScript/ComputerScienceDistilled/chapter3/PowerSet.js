5c50594121bfc7132ca7d6ca93608b691813149b
Gabriel1000homem <gabrielpacmil@gmail.com>
2022-01-30 01:55:11 -0300

    rerirando logs

--- JavaScript/ComputerScienceDistilled/chapter3/PowerSet.js
+++ JavaScript/ComputerScienceDistilled/chapter3/PowerSet.js
@@ -1,20 +1,13 @@
 function power_set(flowers) {
   let fragrances = new Set();
   fragrances.add(new Set());
-  console.log("fragrances:");
-  console.log(fragrances);
   flowers.forEach((flower) => {
-    console.log("flower: " + flower);
     let new_fragrances = fragranceCopy(fragrances);
     new_fragrances.forEach((fragrance) => {
-      console.log("fragrance:");
-      console.log(fragrance);
       fragrance.add(flower);
     });
     new_fragrances.add(flower);
     fragrances.add(new_fragrances);
-    console.log("fragrances:");
-    console.log(fragrances);
   });
 
   return fragrances;
@@ -36,12 +29,12 @@ function fragranceCopy(fragrances) {
   return result;
 }
 
-let flowers = ["A", "B", "C", "D"];
+let flowers = ["A", "B", "C", "D", "E"];
 console.log("flowers: " + flowers);
 let set = power_set(flowers);
 console.log("fragrances: ");
 console.log(set);
-let size = 0;
+let size = 1;
 set.forEach((element) => {
   size += element.size;
 });
