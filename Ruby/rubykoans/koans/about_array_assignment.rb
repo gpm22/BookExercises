7bb715f14b993765f14d622dd3ef9a7a05397b1e
Gabriel Milhomem <gabriel.milhomem@ul.com>
2022-10-07 18:45:44 -0300

    Continuando RubyKoans (22%)

--- Ruby/rubykoans/koans/about_array_assignment.rb
+++ Ruby/rubykoans/koans/about_array_assignment.rb
@@ -3,25 +3,36 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class AboutArrayAssignment < Neo::Koan
   def test_non_parallel_assignment
     names = ["John", "Smith"]
-    assert_equal __, names
+    assert_equal ["John", "Smith"], names
   end
 
   def test_parallel_assignments
     first_name, last_name = ["John", "Smith"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "John", first_name
+    assert_equal "Smith", last_name
   end
 
   def test_parallel_assignments_with_extra_values
     first_name, last_name = ["John", "Smith", "III"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "John", first_name
+    assert_equal "Smith", last_name
   end
 
   def test_parallel_assignments_with_splat_operator
     first_name, *last_name = ["John", "Smith", "III"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "John", first_name
+    assert_equal ["Smith", "III"], last_name
+  end
+
+  def test_parallel_assignments_with_splat_operator_2
+    *last_name = ["John", "Smith", "III"]
+    assert_equal ["John", "Smith", "III"], last_name
+  end
+
+  def test_parallel_assignments_with_splat_operator_3
+    first_name, last_name, *title = ["John", "Smith", "III"]
+    assert_equal "John", first_name
+    assert_equal ["III"], title
   end
 
   def test_parallel_assignments_with_too_few_values
