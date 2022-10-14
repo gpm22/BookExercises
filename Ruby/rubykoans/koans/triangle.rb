ec26b27310fa690ca86a7e56f5af3b397e56d8c2
Gabriel Milhomem <gabriel.milhomem@ul.com>
2022-10-14 18:31:36 -0300

    Continuando RubyKoans (65%)

--- Ruby/rubykoans/koans/triangle.rb
+++ Ruby/rubykoans/koans/triangle.rb
@@ -14,6 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  isATriangle = a + b > c && a + c > b && b + c > a
+  hasNonPositive = a <=0 || b <= 0 || c <=0
+
+  if(hasNonPositive || !isATriangle)
+    raise TriangleError
+  end
+
   if (a == b && a == c)
     :equilateral
   elsif (a == b || a == c || b == c )
