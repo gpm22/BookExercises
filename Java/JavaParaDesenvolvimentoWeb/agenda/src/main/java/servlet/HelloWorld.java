3141ae6915897a2b804586dded5596ebca167861
Gabriel Milhomem <gabriel.milhomem@ul.com>
2023-07-26 17:35:32 -0300

    (Java Web) Adding init and destroy methods and testing racing conditions in servlets

--- Java/JavaParaDesenvolvimentoWeb/agenda/src/main/java/servlet/HelloWorld.java
+++ Java/JavaParaDesenvolvimentoWeb/agenda/src/main/java/servlet/HelloWorld.java
@@ -1,28 +1,42 @@
 package servlet;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HelloWorld extends HttpServlet {
+  private int contador = 0; // variavel de instância
 
-    @Override
-    protected void service(HttpServletRequest request,
-                           HttpServletResponse response)
-            throws ServletException, IOException {
+  @Override
+  protected void service(HttpServletRequest request,
+      HttpServletResponse response)
+      throws ServletException, IOException {
+    contador++; // a cada requisição a mesma variável é incrementada
+    PrintWriter out = response.getWriter();
 
-      PrintWriter out = response.getWriter();
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>Primeira Servlet</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>Oi mundo Servlet 1!</h1>");
+    out.println("Contador agora é: " + contador);
+    out.println("</body>");
+    out.println("</html>");
+  }
 
-      out.println("<html>");
-      out.println("<head>");
-      out.println("<title>Primeira Servlet</title>");
-      out.println("</head>");
-      out.println("<body>");
-      out.println("<h1>Oi mundo Servlet 1!</h1>");
-      out.println("</body>");
-      out.println("</html>");
-    }
+  public void init(ServletConfig config) throws ServletException {
+    super.init(config);
+    log("Iniciando a servlet");
+  }
+
+  public void destroy() {
+    super.destroy();
+    log("Destruindo a servlet");
+  }
 }
