9aff7aa1b54c2fc236b0bc2d63013b240fce33c7
Gabriel Milhomem <gabriel.milhomem@ul.com>
2023-10-27 13:48:33 -0300

    (Java Web) Creating the project-tasks spring project

---
+++ Java/JavaParaDesenvolvimentoWeb/project-tasks/src/main/java/tasks/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package tasks.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/helloWorldSpring")
+    public String execute() {
+        System.out.println("Executando a lógica com Spring MVC");
+        return "index";
+    }
+}
