fefa8e1ab9fd3bda38f3fcb352f60faa0229194a
Gabriel Milhomem <gabrielpacmil@gmail.com>
2024-03-12 18:45:34 -0300

    (ADSA Java) Implementing Shell Sort

--- Java/Developer to Software Architect/Track 1 Developer/Advanced Data Structures & Algorithms in Java/Sorting Algorithms/Sorting.java
+++ Java/Developer to Software Architect/Track 1 Developer/Advanced Data Structures & Algorithms in Java/Sorting Algorithms/Sorting.java
@@ -6,6 +6,8 @@ public class Sorting{
         List<Integer> arr = new ArrayList<>(Arrays.asList(10, 9, 8, 4, 3, 1));
         List<Integer> arr2 = new ArrayList<>(Arrays.asList(10, 9, 8, 4, 3, 1));
         List<Integer> arr3 = new ArrayList<>(Arrays.asList(10, 9, 8, 4, 3, 1));
+        List<Integer> arr4 = new ArrayList<>(Arrays.asList(10, 9, 8, 4, 3, 1));
+        List<Integer> arr5 = new ArrayList<>(Arrays.asList(10, 9, 8, 4, 3, 1));
         System.out.println(arr);
         selectSort(arr);
         System.out.println(arr);
@@ -15,21 +17,37 @@ public class Sorting{
         System.out.println(arr3);
         insertSort(arr3);
         System.out.println(arr3);
+        System.out.println(arr4);
+        shellSort(arr4);
+        System.out.println(arr4);
+        System.out.println(arr5);
+        bubbleShellSort(arr5);
+        System.out.println(arr5);
 
         bubbleSort(arr2);
         selectSort(arr2);
         insertSort(arr2);
+        shellSort(arr2);
+        bubbleShellSort(arr2);
 
 
-        System.out.println("Bubble sort x Insert sort");
+        System.out.println("Bubble sort x Insert sort x Shell Sort");
         List<Integer> arrr1 = new ArrayList<>(Arrays.asList(1, 5, 4, 7, 6, 9, 10, 11));
         List<Integer> arrr2 = new ArrayList<>(Arrays.asList(1, 5, 4, 7, 6, 9, 10, 11));
+        List<Integer> arrr3 = new ArrayList<>(Arrays.asList(1, 5, 4, 7, 6, 9, 10, 11));
+        List<Integer> arrr4 = new ArrayList<>(Arrays.asList(1, 5, 4, 7, 6, 9, 10, 11));
         System.out.println(arrr1);
         bubbleSort(arrr1);
         System.out.println(arrr1);
         System.out.println(arrr2);
         insertSort(arrr2);
         System.out.println(arrr2);
+        System.out.println(arrr3);
+        shellSort(arrr3);
+        System.out.println(arrr3);
+        System.out.println(arrr4);
+        bubbleShellSort(arrr4);
+        System.out.println(arrr4);
     }
 
     private static <T extends Comparable<T>> void selectSort(List<T> list){
@@ -58,28 +76,61 @@ public class Sorting{
                 if(!valueToCompare.equals(list.get(j)))
                     swap = true;
             }
-            if(!swap){
-                System.out.println("bubbleSort steps:" + numberOfSteps + " for n:" + list.size());
-                return;
-            }
+            if(!swap)
+                break;
         }
         System.out.println("bubbleSort steps:" + numberOfSteps + " for n:" + list.size());
     }
-
     private static <T extends Comparable<T>> void  insertSort(List<T> list){
+        insertSort(list, 1);
+    }
+
+    private static <T extends Comparable<T>> void  insertSort(List<T> list, int increment){
         int numberOfSteps = 0;
 
-        for(int j = 1; j < list.size(); j++){
-            for(int i = j; i > 0 ; i--){
+        for(int j = 1; j + increment < list.size(); j++){
+            for(int i = j + increment; i - increment >= 0 ; i -= increment){
                 numberOfSteps++;
                 T valueToCompare = list.get(i);
-                swap(list, i-1, i);
+                swap(list, i-increment, i);
                 if(valueToCompare.equals(list.get(i)))
                     break;
             }
         }
 
         System.out.println("insertionSort steps:" + numberOfSteps + " for n:" + list.size());
+    }
+
+    private static <T extends Comparable<T>> void  bubbleShellSort(List<T> list){
+        int numberOfSteps = 0;
+        int step = list.size()/2;
+
+        while(true){
+            for(int i = 0; i < list.size(); i++){
+                if(i+step > list.size()-1)
+                    break;
+                numberOfSteps++;
+                swap(list, i, i+step);
+            }
+
+            if(step == 1)
+                break;
+
+            step = (int) Math.ceil(step/2.0);
+        }
+        System.out.println("bubbleShell steps:" + numberOfSteps + " for n:" + list.size());
+    }
+
+
+    private static <T extends Comparable<T>> void  shellSort(List<T> list){
+        int numberOfSteps = 0;
+        int increment = list.size()/2;
+        while(increment >= 1){
+            numberOfSteps++;
+            insertSort(list, increment);
+            increment /= 2;
+        }
+        System.out.println("shellSort steps:" + numberOfSteps + " for n:" + list.size());
     }
 
     private static <T extends Comparable<T>> void swap(List<T> list, int firstIndex, int secondIndex){
