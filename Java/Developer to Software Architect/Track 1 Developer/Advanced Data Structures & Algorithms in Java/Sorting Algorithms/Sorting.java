c75a898aaa2e0e5b5c4168972a78ecd5d8da4f6e
Gabriel Milhomem <gabrielpacmil@gmail.com>
2024-03-12 10:53:58 -0300

    (ADSA Java) Implementing Bubble Sort

--- Java/Developer to Software Architect/Track 1 Developer/Advanced Data Structures & Algorithms in Java/Sorting Algorithms/Sorting.java
+++ Java/Developer to Software Architect/Track 1 Developer/Advanced Data Structures & Algorithms in Java/Sorting Algorithms/Sorting.java
@@ -4,23 +4,57 @@ public class Sorting{
 
     public static void main(String[] args) {
         List<Integer> arr = new ArrayList<>(Arrays.asList(10, 9, 8, 4, 3, 1));
+        List<Integer> arr2 = new ArrayList<>(Arrays.asList(10, 9, 8, 4, 3, 1));
         System.out.println(arr);
         selectSort(arr);
         System.out.println(arr);
+        System.out.println(arr2);
+        bubbleSort(arr2);
+        System.out.println(arr2);
+        bubbleSort(arr2);
+        selectSort(arr2);
     }
 
     private static <T extends Comparable<T>> void selectSort(List<T> list){
 
+        int numberOfSteps = 0;
         for(int i = 0; i<list.size(); i++){
             for(int j = i + 1; j< list.size(); j++){
-                T firstValue = list.get(i);
-                T secondValue = list.get(j);
-                if(secondValue.compareTo(firstValue) < 0){
-                    list.set(i, secondValue);
-                    list.set(j, firstValue);
-                }
+                numberOfSteps++;
+                swap(list, i, j);
             }
         }
+        System.out.println("selectSort steps:" + numberOfSteps+ " for n:" + list.size());
     }
 
+    private static <T extends Comparable<T>> void bubbleSort(List<T> list){
+
+        boolean swap;
+        int numberOfSteps = 0;
+        for(int i = 0; i <list.size(); i++){
+            swap = false;
+            for(int j = 0; j < list.size()-1;j++){
+                numberOfSteps++;
+                T valueToCompare = list.get(j);
+                swap(list, j, j+1);
+
+                if(!valueToCompare.equals(list.get(j)))
+                    swap = true;
+            }
+            if(!swap){
+                System.out.println("bubbleSort steps:" + numberOfSteps + " for n:" + list.size());
+                return;
+            }
+        }
+        System.out.println("bubbleSort steps:" + numberOfSteps + " for n:" + list.size());
+    }
+
+    private static <T extends Comparable<T>> void swap(List<T> list, int firstIndex, int secondIndex){
+        T firstValue = list.get(firstIndex);
+        T secondValue = list.get(secondIndex);
+        if(secondValue.compareTo(firstValue) < 0){
+            list.set(firstIndex, secondValue);
+            list.set(secondIndex, firstValue);
+        }
+    }
 }
