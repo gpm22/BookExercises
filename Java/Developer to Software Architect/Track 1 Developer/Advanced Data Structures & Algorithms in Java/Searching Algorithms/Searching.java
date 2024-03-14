7000331f048204e7da6473c3c92f98d1e8540171
Gabriel Milhomem <gabrielpacmil@gmail.com>
2024-03-14 17:23:54 -0300

    (ADSA Java) Implementing searching algorithms

---
+++ Java/Developer to Software Architect/Track 1 Developer/Advanced Data Structures & Algorithms in Java/Searching Algorithms/Searching.java
@@ -0,0 +1,126 @@
+import java.util.*;
+import java.util.function.*;
+
+public class Searching {
+
+    public static void main(String[] args) {
+        
+        List<BiFunction<List<Integer>, Integer, Integer>> SEARCHS = Arrays.asList(
+            (list, element) -> linearSearch(list, element),
+            (list, element) -> binarySearch(list, element),
+            (list, element) -> jumpSearch(list, element),
+            (list, element) -> interpolationSearch(list, element)
+        );
+
+
+        for(BiFunction<List<Integer>, Integer, Integer> fun : SEARCHS)
+            test(fun);
+    }
+
+    static private int testNumber = 0;
+    private static void test(BiFunction<List<Integer>, Integer, Integer> fun){
+
+        testNumber++;
+        System.out.println("\n Test number: " + testNumber + "\n");
+        List<Integer> list = Arrays.asList(1, 4, 6, 7, 12, 14, 16);
+
+        Integer result;
+        System.out.println("Test last element");
+        result = fun.apply(list, list.get(list.size()-1));
+        System.out.println("correct? " + (result == list.size()-1));
+
+        System.out.println("Test first element");
+        result = fun.apply(list, list.get(0));
+        System.out.println("correct? " + (result == 0));
+
+        System.out.println("Test middle element");
+        result = fun.apply(list, list.get(list.size()/2));
+        System.out.println("correct? " + (result ==list.size()/2));
+
+        System.out.println("Test non existent element bellow");
+        result = fun.apply(list, -12);
+        System.out.println("correct? " + (result == -1));
+
+        System.out.println("Test non existent element above");
+        result = fun.apply(list, 40);
+        System.out.println("correct? " + (result == -1));
+    }
+    
+    private static <T> int linearSearch(List<T> list, T element){
+        for(int i = 0; i < list.size(); i++)
+            if (list.get(i) == element)
+                return i;
+        
+        return-1 ;
+    }
+    
+    private static <T extends Comparable<T>> int binarySearch(List<T> list, T element){
+
+        int low = 0;
+        int high = list.size()-1;
+        int max = list.size();
+        int count = 0;
+        while(low <= high){
+            count++;
+            int mid = (high-low)/2+low;
+            T testElement = list.get(mid);
+            if(testElement.equals(element))
+                return mid;
+            
+            if(testElement.compareTo(element) > 0){
+                high = mid-1;
+            } else {
+                low = mid + 1;
+            }
+
+            if(count > max)
+                throw new RuntimeException("list is not ascended sorted");
+        }
+
+        return -1;
+    }
+
+    private static <T extends Comparable<T>> int jumpSearch(List<T> list, T element){
+        int jumptLength = (int) Math.floor(Math.sqrt(list.size()));
+        return jumpSearch(list, element, jumptLength);
+    }
+
+    private static <T extends Comparable<T>> int jumpSearch(List<T> list, T element, int jumpLength){
+
+    int i;
+    for(i=0; i < list.size() && list.get(i).compareTo(element) <= 0; i+=jumpLength);
+
+    int startIndex = i - jumpLength;
+    if(startIndex < 0)
+        startIndex = 0;
+
+    int endIndex = Math.min(i, list.size());
+    
+    for (int j = startIndex; j <endIndex; j++)
+        if(list.get(j).equals(element))
+            return j;
+    
+    return -1;
+}
+    private static int interpolationSearch(List<Integer> list, Integer element){
+        int low = 0;
+        int high = list.size() -1;
+        while(low <= high){
+            int mid = low + ((element - list.get(low))*(high-low))/(list.get(high) - list.get(low));
+
+            if(mid < 0 | mid > list.size() - 1)
+                return -1;
+        
+            if(list.get(mid) == element)
+                return mid;
+        
+            if(list.get(mid) < element){
+                low = mid + 1;
+            } else {
+                high = mid -1;
+            }
+        }
+    
+        return -1;
+    }
+}
