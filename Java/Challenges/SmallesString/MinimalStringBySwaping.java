b7e03da4d59fb12c966f2d2d3e468e83d7090703
Gabriel Milhomem <gabrielpacmil@gmail.com>
2024-03-19 11:29:17 -0300

    (Java Challenge) Implementing linear version of algorithm

--- Java/Challenges/SmallesString/MinimalStringBySwaping.java
+++ Java/Challenges/SmallesString/MinimalStringBySwaping.java
@@ -33,7 +33,10 @@ class MinimalStringBySwaping {
 
     private static void comparingTest(String name, String str, String expected) {
         long quadratic = testQuadratic(name, str, expected);
+        long linearOnAlphabet = testLinearOnAlphabet(name, str, expected);
         long linear = testLinear(name, str, expected);
+        System.out.println("linear on alphabet/quadratic %" + ((linearOnAlphabet * 100.0) / quadratic));
+        System.out.println("linear/linear on alphabet %" + ((linear * 100.0) / linearOnAlphabet));
         System.out.println("linear/quadratic %" + ((linear * 100.0) / quadratic));
     }
 
@@ -42,6 +45,14 @@ class MinimalStringBySwaping {
         String ans = getMinimalStringQuadratic(str);
         long endTime = System.nanoTime();
         testMessage("Quadratic " + name, str, expected, ans, (endTime - initTime));
+        return endTime - initTime;
+    }
+
+    private static long testLinearOnAlphabet(String name, String str, String expected) {
+        long initTime = System.nanoTime();
+        String ans = getMinimalStringLinearOnAlphabet(str);
+        long endTime = System.nanoTime();
+        testMessage("Linear On Alphabet " + name, str, expected, ans, (endTime - initTime));
         return endTime - initTime;
     }
 
@@ -63,7 +74,7 @@ class MinimalStringBySwaping {
                     "\tIt is - \n" + ans + "\n\tBut should be - \n" + expected + "\n\toriginal - \n" + str);
     }
 
-    // Algorithms is O(m^2*n), where n is the string length and m is the alphabet
+    // Algorithm is O(m^2*n), where n is the string length and m is the alphabet
     // length
     public static String getMinimalStringQuadratic(String str) {
 
@@ -80,9 +91,9 @@ class MinimalStringBySwaping {
         return ans;
     }
 
-    // Algorithms is O(m log m + n*m), where n is the string length and m is the
+    // Algorithm is O(m log m + n*m), where n is the string length and m is the
     // alphabet length, in the case n >> m, it is O(n*m)
-    public static String getMinimalStringLinear(String str) {
+    public static String getMinimalStringLinearOnAlphabet(String str) {
         Map<Character, Integer> map = mapCorrectOrder(str);
         String ans = str;
 
@@ -95,6 +106,36 @@ class MinimalStringBySwaping {
 
         return ans;
 
+    }
+
+    // Algorithm is O(m log m + n), where n is the string length and m is the
+    // alphabet length, in the case n >> m, it is O(n)
+    public static String getMinimalStringLinear(String str){
+        Map<Character, Character> mapChars = mapOldToNewChar(str);
+        return getSwappedString(str, mapChars);
+    }
+
+    private static Map<Character, Character> mapOldToNewChar(String str){
+        Map<Character, Integer> mapCorrectOrder = mapCorrectOrder(str);
+        Map<Character, Character> map = new HashMap<>();
+        for(Entry<Character, Integer> entry: mapCorrectOrder.entrySet())
+            map.put(str.charAt(entry.getValue()), entry.getKey());
+
+        return map;
+    }
+
+    private static Map<Character, Integer> mapCorrectOrder(String str) {
+        return mapCorrectOrder(mapCharPos(str));
+    }
+
+    private static Map<Character, Integer> mapCorrectOrder(Map<Character, Integer> mapCharPos) {
+        Map<Character, Integer> newMap = new HashMap<>();
+        List<Character> sortedKeys = getSortedKeys(mapCharPos);
+        List<Integer> sortedValues = getSortedValues(mapCharPos);
+
+        for (int i = 0; i < sortedKeys.size(); i++)
+            newMap.put(sortedKeys.get(i), sortedValues.get(i));
+        return newMap;
     }
 
     private static Map<Character, Integer> mapCharPos(String str) {
@@ -113,28 +154,23 @@ class MinimalStringBySwaping {
 
     }
 
-    private static Map<Character, Integer> mapCorrectOrder(String str) {
-        return mapCorrectOrder(mapCharPos(str));
+    private static String getSwappedString(String str, Map<Character, Character> mapOldToNew){
+        char[] chars = str.toCharArray();
+
+        for(int i =0; i < chars.length; i++)
+            chars[i] = mapOldToNew.get(chars[i]);
+
+        return String.valueOf(chars);
     }
 
-    private static Map<Character, Integer> mapCorrectOrder(Map<Character, Integer> mapCharPos) {
-        Map<Character, Integer> newMap = new HashMap<>();
-        List<Character> sortedKeys = getSortedKeys(mapCharPos);
-        List<Integer> sortedValues = getSortedValues(mapCharPos);
-
-        for (int i = 0; i < sortedKeys.size(); i++)
-            newMap.put(sortedKeys.get(i), sortedValues.get(i));
-        return newMap;
-    }
-
-    public static List<Integer> getSortedValues(Map<Character, Integer> map) {
+    private static List<Integer> getSortedValues(Map<Character, Integer> map) {
         List<Integer> list = new ArrayList<>(map.values());
 
         Collections.sort(list);
         return list;
     }
 
-    public static List<Character> getSortedKeys(Map<Character, Integer> map) {
+    private static List<Character> getSortedKeys(Map<Character, Integer> map) {
         List<Character> list = new ArrayList<>(map.keySet());
 
         Collections.sort(list);
