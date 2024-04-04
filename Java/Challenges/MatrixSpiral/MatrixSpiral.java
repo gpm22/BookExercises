d8ed1a239e65d471f4a2310aed62e87d73c92ece
Gabriel Milhomem <gabrielpacmil@gmail.com>
2024-04-04 10:56:38 -0300

    Concluding matrix spiral

--- Java/Challenges/MatrixSpiral/MatrixSpiral.java
+++ Java/Challenges/MatrixSpiral/MatrixSpiral.java
@@ -1,15 +1,39 @@
 public class MatrixSpiral {
 
+    static int testNumber = 0;
+
     public static void main(String[] args) {
         String[] arr1 = new String[] { "1,2,3", "4,5,6", "7,8,9" };
-        System.out.println(createSpiral(arr1));
+        test(arr1, "1,2,3,6,9,8,7,4,5");
+
+        String[] arr2 = new String[] { "1,2", "4,5"};
+        test(arr2, "1,2,5,4");
+
+        String[] arr3 = new String[] { "1, 2, 3, 4", "5, 6, 7, 8"};
+        test(arr3, "1,2,3,4,8,7,6,5");
+
+        String[] arr4 = new String[] { "1, 2", "3, 4", "5, 6", "7, 8"};
+        test(arr4, "1,2,4,6,8,7,5,3");
+
+        String[] arr5 = new String[] { "1,2,3, 4", "5,6, 7, 8", "9, 10, 11, 12", "13, 14, 15, 16" };
+        test(arr5, "1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10");
+    }
+
+    private static void test(String[] input, String result){
+        String current = createSpiral(input);
+        testNumber++;
+        if(!current.equals(result))
+            throw new RuntimeException("test " + testNumber +  " failed! " +  current + " is different from " + result);
+
+        System.out.println("test " + testNumber + " passed!");
+        System.out.println(current);
     }
 
     public static String createSpiral(String[] arrStr) {
 
         String[][] matrix = getMatrix(arrStr);
         printMatrix(matrix);
-        System.out.println("----");
+        System.out.println("\n----");
         String result = "";
         // print first row, last column, last row inversed, firs column inversed
 
@@ -19,39 +43,30 @@ public class MatrixSpiral {
         int lastColumn = matrix[0].length - 1;
 
         while (firstRow <= lastRow && firstColumn <= lastColumn) {
-            System.out.println("1");
-            System.out.println(result);
             // printFirstRow
             for (int i = firstColumn; i <= lastColumn; i++)
-                result += result + "," + matrix[firstRow][i];
+                result += "," + matrix[firstRow][i];
 
-            System.out.println("2");
-            System.out.println(result);
             // printLastColumn
             for (int i = firstRow + 1; i < lastRow; i++)
-                result += result + "," + matrix[i][lastColumn];
+                result += "," + matrix[i][lastColumn];
 
-            System.out.println("3");
-            System.out.println(result);
             // printLastRow
-            for (int i = lastColumn; i >= firstColumn; i--)
-                result += result + "," + matrix[lastRow][i];
+            if(firstRow != lastRow)
+                for (int i = lastColumn; i >= firstColumn; i--)
+                    result += "," + matrix[lastRow][i];
 
-            System.out.println("4");
-            System.out.println(result);
             // printFirstColumn
-            for (int i = lastRow - 1; i > firstRow; i--)
-                result += result + "," + matrix[i][firstColumn];
+            if(lastColumn != firstColumn)
+                for (int i = lastRow - 1; i > firstRow; i--)
+                    result += "," + matrix[i][firstColumn];
 
-            System.out.println("5");
-            System.out.println(result);
-            System.out.println("---------------");
             firstRow++;
             lastRow--;
             firstColumn++;
             lastColumn--;
         }
-        return result;
+        return result.substring(1);
     }
 
     private static String[][] getMatrix(String[] arrStr) {
@@ -59,7 +74,7 @@ public class MatrixSpiral {
         String[][] result = new String[arrStr.length][];
 
         for (int i = 0; i < arrStr.length; i++) {
-            String[] row = arrStr[i].split(",");
+            String[] row = arrStr[i].replaceAll("\\s", "").split(",");
             result[i] = new String[row.length];
             for (int j = 0; j < row.length; j++)
                 result[i][j] = row[j];
@@ -70,8 +85,11 @@ public class MatrixSpiral {
 
     private static void printMatrix(String[][] matrix) {
 
-        for (String[] arr : matrix)
+        for (String[] arr : matrix){
+            System.err.println();
             for (String str : arr)
-                System.out.print(str);
+                System.out.print(" " + str);
+        }
+
     }
 }
