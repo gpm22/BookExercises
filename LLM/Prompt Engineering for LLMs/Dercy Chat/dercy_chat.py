ee67be9fbacad1146fe53d45c131b847553d62ac
Gabriel1000homem <gabrielpacmil@gmail.com>
2025-08-15 01:34:09 -0300

    (Darcy Chat) adding get_wikipedia_page function and tool

--- LLM/Prompt Engineering for LLMs/Dercy Chat/dercy_chat.py
+++ LLM/Prompt Engineering for LLMs/Dercy Chat/dercy_chat.py
@@ -1,4 +1,6 @@
 from openai import OpenAI
+from requests import get
+from googlesearch import search
 
 goodbye_list = ["tchau", "adeus", "até mais", "ate mais", "até logo", "ate logo", "até breve", "ate breve"]
 
@@ -17,11 +19,38 @@ def get_prompt():
     dercy = "Dercy Gonçalves"
     
     return "\n".join(contents).replace("você", dercy).replace("Você", dercy)
-
+    
+def get_wikipedia_page(search_item):
+    for wiki_page in search(search_item + " site:wikipedia.org", tld="co.in", num=1, stop=1, pause=2):
+        response = get(wiki_page+"?action=raw")
+        if response.status_code == 200:
+            return response.text
+        else:
+            print(f"Failed to fetch page {wiki_page}, status code: {response.status_code}")
+            return ""
+        
 client = OpenAI()
 model = "gpt-3.5-turbo"
 messages=[
     {"role": "system", "content" : "Você é a Dercy Gonçalves. Você tem bom humor e fala palavrões frequentemente. Você usa ao menos um xigamento em cada frase. Você sempre responde corretamente o que foi pedido. Seus argumentos sempre são baseados em ciência e lógica."}
+]
+
+tools = [
+    { 
+        "type": "function",
+        "name": "get_wikipedia_page",
+        "description": "Retorna a página da wikipedia mais próxima ao input dado",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "search_item": {
+                    "type": "string",
+                    "description": "Input ao qual a página da wikipedia será retornado.",
+                },
+            },
+            "required": ["search_item"],
+        },
+    },
 ]
 
 print("---------------------------")
