7fd5aca8f9861958eec42252f741353b9531cbad
Gabriel1000homem <gabrielpacmil@gmail.com>
2026-03-07 19:53:11 -0300

    (Flutter State) adding ArticleModel

--- Flutter/Tutorial/wikipedia_reader/lib/main.dart
+++ Flutter/Tutorial/wikipedia_reader/lib/main.dart
@@ -18,20 +18,35 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.black87,
-        textTheme: Theme.of(context).textTheme.apply(
-          bodyColor: Colors.amber,
-          displayColor: Colors.purple,
-        ),
-        
+        textTheme: Theme.of(
+          context,
+        ).textTheme.apply(bodyColor: Colors.amber, displayColor: Colors.purple),
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Wikipedia Flutter'),
-        ),
-        body: const Center(
-          child: Text('Loading...'),
-        ),
+        appBar: AppBar(title: const Text('Wikipedia Flutter')),
+        body: const Center(child: Text('Loading...')),
       ),
     );
+  }
+}
+
+class ArticleModel {
+  // todo
+
+  Future<Summary> getRandomArticleSUmmary() async {
+    final uri = Uri.https(
+      'en.wikipedia.org',
+      '/api/rest_v1/page/random/summary',
+    );
+
+    final response = await get(uri);
+
+    if (response.statusCode != 200) {
+      throw HttpException(
+        'Failed to fetch random article.\nstatus code=${response.statusCode}\nbody:${response.body}',
+      );
+    }
+
+    return Summary.fromJson(jsonDecode(response.body));
   }
 }
