7a5f8eb7d7d037dab78c2101842dda8c475c74a7
Gabriel1000homem <gabrielpacmil@gmail.com>
2026-03-11 02:18:13 -0300

    (Flutter State) Concluding tutorial

--- Flutter/Tutorial/wikipedia_reader/lib/main.dart
+++ Flutter/Tutorial/wikipedia_reader/lib/main.dart
@@ -14,25 +14,18 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) => MaterialApp(
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.black87,
         textTheme: Theme.of(
           context,
         ).textTheme.apply(bodyColor: Colors.amber, displayColor: Colors.purple),
       ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Wikipedia Flutter')),
-        body: const Center(child: Text('Loading...')),
-      ),
-    );
-  }
+    home: ArticleView(),
+  );
 }
 
 class ArticleModel {
-  // todo
-
   Future<Summary> getRandomArticleSUmmary() async {
     final uri = Uri.https(
       'en.wikipedia.org',
@@ -49,4 +42,113 @@ class ArticleModel {
 
     return Summary.fromJson(jsonDecode(response.body));
   }
+}
+
+class ArticleViewModel extends ChangeNotifier {
+  final ArticleModel model;
+  Summary? summary;
+  String? errorMessage;
+  bool loading = false;
+
+  ArticleViewModel(this.model) {
+    getRandomArticleSummary();
+  }
+
+  Future<void> getRandomArticleSummary() async {
+    loading = true;
+    notifyListeners();
+
+    try {
+      summary = await model.getRandomArticleSUmmary();
+      errorMessage = null;
+    } on HttpException catch (error) {
+      errorMessage = error.message;
+      summary = null;
+    }
+
+    loading = false;
+    notifyListeners();
+  }
+}
+
+class ArticleView extends StatelessWidget {
+  ArticleView({super.key});
+
+  final viewModel = ArticleViewModel(ArticleModel());
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(title: const Text('Wikipedia Flutter')),
+    body: ListenableBuilder(
+      listenable: viewModel,
+      builder: (context, child) => switch ((
+        viewModel.loading,
+        viewModel.summary,
+        viewModel.errorMessage,
+      )) {
+        (true, _, _) => CircularProgressIndicator(),
+        (false, _, String message) => Center(child: Text(message)),
+        (false, null, null) => Center(
+          child: Text("An unkown error has occured"),
+        ),
+        (false, Summary summary, null) => ArticlePage(
+          summary: summary,
+          nextArticleCallback: viewModel.getRandomArticleSummary,
+        ),
+      },
+    ),
+  );
+}
+
+class ArticlePage extends StatelessWidget {
+  const ArticlePage({
+    super.key,
+    required this.summary,
+    required this.nextArticleCallback,
+  });
+
+  final Summary summary;
+  final VoidCallback nextArticleCallback;
+
+  @override
+  Widget build(BuildContext context) => SingleChildScrollView(
+    child: Column(
+      children: [
+        ArticleWidget(summary: summary),
+        ElevatedButton(
+          onPressed: nextArticleCallback,
+          child: Text('Next random article'),
+        ),
+      ],
+    ),
+  );
+}
+
+class ArticleWidget extends StatelessWidget {
+  const ArticleWidget({super.key, required this.summary});
+
+  final Summary summary;
+
+  @override
+  Widget build(BuildContext context) => Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Column(
+      spacing: 10.0,
+      children: [
+        if (summary.hasImage) Image.network(summary.originalImage!.source),
+        Text(
+          summary.titles.normalized,
+          overflow: TextOverflow.ellipsis,
+          style: TextTheme.of(context).displaySmall,
+        ),
+        if (summary.description != null)
+          Text(
+            summary.description!,
+            overflow: TextOverflow.ellipsis,
+            style: TextTheme.of(context).bodySmall,
+          ),
+        Text(summary.extract),
+      ],
+    ),
+  );
 }
